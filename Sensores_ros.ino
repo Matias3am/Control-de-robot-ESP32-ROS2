dc8bfdef3361f039e6063ac7583a9e0d05dbcb1c
PedrodiazH <133780341+PedrodiazH@users.noreply.github.com>
2024-07-31 23:39:00 -0400

    Update Sensores_ros.ino

--- Sensores_ros.ino
+++ Sensores_ros.ino
@@ -1,38 +1,38 @@
-#include <micro_ros_arduino.h>  // Incluye la biblioteca de micro-ROS para Arduino
-#include <stdio.h>             // Biblioteca estándar para funciones de entrada/salida
-#include <rcl/rcl.h>           // Biblioteca de comunicación de ROS 2
-#include <rcl/error_handling.h> // Biblioteca de manejo de errores de ROS 2
-#include <rclc/rclc.h>         // Biblioteca para la inicialización de ROS 2 en C
-#include <rclc/executor.h>     // Ejecutor para manejar la ejecución de nodos en ROS 2
-#include <std_msgs/msg/float32.h> // Biblioteca de mensajes estándar para tipo float32
-#include <sensor_msgs/msg/imu.h>  // Biblioteca de mensajes para IMU (unidad de medición inercial)
-#include <sensor_msgs/msg/range.h>// Biblioteca de mensajes para rango de sensores
-#include <rosidl_runtime_c/string.h> // Manejo de cadenas en ROS 2
+#include <micro_ros_arduino.h>  
+#include <stdio.h>             
+#include <rcl/rcl.h>           
+#include <rcl/error_handling.h> 
+#include <rclc/rclc.h>        
+#include <rclc/executor.h>     
+#include <std_msgs/msg/float32.h> 
+#include <sensor_msgs/msg/imu.h>  
+#include <sensor_msgs/msg/range.h>
+#include <rosidl_runtime_c/string.h> 
 #include "std_msgs/msg/detail/header__struct.h"
-#include <std_msgs/msg/int32.h> // Biblioteca de mensajes estándar para tipo int32
+#include <std_msgs/msg/int32.h>
 
-// Pines utilizados para el sensor ultrasónico
-const int pinTrig = 5; // Pin de disparo del sensor ultrasónico
-const int pinEcho = 18; // Pin de eco del sensor ultrasónico
+// Pines de sensor ultrasinico
+const int pinTrig = 5; // disparo del sensor
+const int pinEcho = 18; // eco del sensor
 
-// Constantes para la conversión de distancia
-#define VELOCIDAD_SONIDO 0.034 // Velocidad del sonido en cm/µs
-#define DISTANCIA_METROS 0.01 // Conversión de cm a metros 
+// Conversión de distancia
+#define VELOCIDAD_SONIDO 0.034 
+#define DISTANCIA_METROS 0.01 
 
-// Variables globales para la comunicación con ROS 2
-rcl_publisher_t publicador_distancia; // Publicador para mensajes de distancia
-sensor_msgs__msg__Range msg_distancia; // Mensaje para el rango del sensor
+
+rcl_publisher_t publicador_distancia; 
+sensor_msgs__msg__Range msg_distancia; 
 rosidl_runtime_c__String frame_id;
 std_msgs__msg__Header header; 
-rclc_executor_t ejecutor; // Ejecutor para manejar temporizadores y callbacks
-rclc_support_t soporte; // Soporte para la inicialización de ROS 2
-rcl_allocator_t asignador; // Asignador de memoria para ROS 2
-rcl_node_t nodo; // Nodo de ROS 2
-rcl_timer_t temporizador; // Temporizador para callbacks periódicos
-rcl_subscription_t subscriber; // Suscriptor para recibir mensajes
-std_msgs__msg__Int32 msg; // Mensaje para suscripción
+rclc_executor_t ejecutor; 
+rclc_support_t soporte; 
+rcl_allocator_t asignador; 
+rcl_node_t nodo; 
+rcl_timer_t temporizador; 
+rcl_subscription_t subscriber; 
+std_msgs__msg__Int32 msg; 
 
-// Variables para el control de motores en el ESP32
+// Pines de control de driver L9110s
 const int a1a = 13;
 const int a1b = 12; 
 const int b1a = 14;
@@ -42,47 +42,45 @@ int velocidad = 85; // Valor de velocidad de 0 a 255
 
 #define LED_PIN 13 // Pin para el LED que indica errores
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}} // Macro para verificar el resultado de las funciones de ROS 2 y manejar errores
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}} 
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-// Función que entra en un bucle infinito en caso de error, parpadeando el LED
 void error_loop(){
   while(1){
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // Cambia el estado del LED
-    delay(100); // Espera 100 ms
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN)); 
+    delay(100); 
   }
 }
 
-// Función para leer la distancia del sensor ultrasónico
+// Función para leer la distancia del sensor 
 float leerDistanciaUltrasonica() {
-  long duracion; // Variable para almacenar la duración del pulso
-  float distanciaCm; // Distancia en centímetros
+  long duracion; 
+  float distanciaCm; 
   float distanciaMetros;
 
-  // Genera el pulso de disparo
+  // Pulso de disparo
   digitalWrite(pinTrig, LOW);
   delayMicroseconds(2);
   digitalWrite(pinTrig, HIGH);
   delayMicroseconds(10);
   digitalWrite(pinTrig, LOW);
 
-  // Lee la duración del pulso de eco
+  // Lee la duracion del pulso de eco
   duracion = pulseIn(pinEcho, HIGH);
-  distanciaCm = duracion * VELOCIDAD_SONIDO / 2; // Calcula la distancia en centímetros
+  distanciaCm = duracion * VELOCIDAD_SONIDO / 2; 
   distanciaMetros = distanciaCm * DISTANCIA_METROS;
-  return distanciaMetros; // Retorna la distancia en metros
+  return distanciaMetros; 
 }
 
-// Función de callback que se ejecuta cada vez que el temporizador se activa
 void temporizador_callback(rcl_timer_t * timer, int64_t last_call_time) {
   if (timer != NULL) {
-    float distancia = leerDistanciaUltrasonica(); // Lee la distancia del sensor ultrasónico
-    msg_distancia.range = distancia; // Asigna la distancia al mensaje
-    RCSOFTCHECK(rcl_publish(&publicador_distancia, &msg_distancia, NULL)); // Publica el mensaje de distancia
+    float distancia = leerDistanciaUltrasonica(); 
+    msg_distancia.range = distancia;
+    RCSOFTCHECK(rcl_publish(&publicador_distancia, &msg_distancia, NULL)); 
   }
 }
 
-// Función de callback para la suscripción
+// Funcin callback suscripcion para manejo de motores del movil
 void subscription_callback(const void * msgin) {  
   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
   if (msg->data == 0){ 
@@ -117,59 +115,52 @@ void subscription_callback(const void * msgin) {
   }
 }
 
-// Función de configuración inicial
 void setup() {
-  set_microros_transports(); // Inicializa los transportes de micro-ROS
-  pinMode(LED_PIN, OUTPUT); // Configura el pin del LED como salida
-  digitalWrite(LED_PIN, HIGH); // Enciende el LED
-  pinMode(pinTrig, OUTPUT); // Configura el pin de disparo del sensor ultrasónico como salida
-  pinMode(pinEcho, INPUT); // Configura el pin de eco del sensor ultrasónico como entrada
+  set_microros_transports(); 
+  pinMode(LED_PIN, OUTPUT); 
+  digitalWrite(LED_PIN, HIGH); 
+  pinMode(pinTrig, OUTPUT); 
+  pinMode(pinEcho, INPUT); 
 
-  // Inicializa el asignador y el soporte para ROS 2
   asignador = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(&soporte, 0, NULL, &asignador));
   RCCHECK(rclc_node_init_default(&nodo, "esp32_valor_ultra", "", &soporte));
 
-  // Inicializa el publicador para el mensaje de distancia
+
   RCCHECK(rclc_publisher_init_default(
     &publicador_distancia,
     &nodo,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Range),
     "distancia_ultrasonica"));
 
-  // Inicializa el suscriptor
   RCCHECK(rclc_subscription_init_default(
     &subscriber,
     &nodo,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
     "diferencial_control_remoto"));
 
-  // Inicializa el temporizador
-  const unsigned int temporizador_timeout = 1000; // Tiempo de espera en milisegundos
+  const unsigned int temporizador_timeout = 1000; 
   RCCHECK(rclc_timer_init_default(
     &temporizador,
     &soporte,
     RCL_MS_TO_NS(temporizador_timeout),
     temporizador_callback));
 
-  // Inicializa el ejecutor y agrega el temporizador y suscriptor
   RCCHECK(rclc_executor_init(&ejecutor, &soporte.context, 2, &asignador));
   RCCHECK(rclc_executor_add_timer(&ejecutor, &temporizador));
   RCCHECK(rclc_executor_add_subscription(&ejecutor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 
-  // Configura el mensaje de distancia
   frame_id.data = "sensor_link";
   header.frame_id = frame_id;
   msg_distancia.header = header;
   msg_distancia.radiation_type = sensor_msgs__msg__Range__ULTRASOUND;
-  msg_distancia.field_of_view = 0.14; // Campo de visión en radianes
-  msg_distancia.min_range = 0.02; // Rango mínimo en metros
-  msg_distancia.max_range = 3.0; // Rango máximo en metros
+  msg_distancia.field_of_view = 0.14; 
+  msg_distancia.min_range = 0.02; 
+  msg_distancia.max_range = 3.0; 
 }
 
-// Función principal que se ejecuta en bucle
 void loop() {
-  RCSOFTCHECK(rclc_executor_spin_some(&ejecutor, RCL_MS_TO_NS(100))); // Ejecuta el ejecutor
-  delay(100); // Espera 100 ms
+  RCSOFTCHECK(rclc_executor_spin_some(&ejecutor, RCL_MS_TO_NS(100))); 
+  delay(100); 
 }
 
